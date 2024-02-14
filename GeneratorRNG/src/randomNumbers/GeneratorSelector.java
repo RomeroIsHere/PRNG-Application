5c11cad9bfbcacfdf5758d8edc90e9199943ca11
Romero <guillermoromecu@gmail.com>
2024-02-14 10:11:11 -0600

    Added Linear congruence

--- GeneratorRNG/src/randomNumbers/GeneratorSelector.java
+++ GeneratorRNG/src/randomNumbers/GeneratorSelector.java
@@ -5,20 +5,19 @@ package randomNumbers;
 public class GeneratorSelector {
 
 	public static void main(String[] args) {
-		// TODO Add Gui
+		// TODO Add GUI
 		MiddleSquare MidSq=new MiddleSquare();
 		MiddleProduct midProd=new MiddleProduct();
 		ConstantMultiplier constantMultiplier=new ConstantMultiplier();
+		LinearCongruence linearC=new LinearCongruence(); 
 		MidSq.setSeed(3137);
 		midProd.setSeed(15367,92463);
 		constantMultiplier.setSeed(62473);
 		constantMultiplier.setMultiplier(13741);
-		//ArrayList<RandomGenerator> list=new ArrayList<RandomGenerator>();
-		//for(RandomGenerator rng:list) {
-			for (int i = 0; i < 6; i++) {	
-				System.out.printf("%f%n",constantMultiplier.next());
+		linearC.setConstants(57,43, 93, 8);
+			for (int i = 0; i < 8; i++) {	
+				System.out.printf("%f%n",linearC.next());
 			}
-		//}
 	}
 
 }
--- GeneratorRNG/src/randomNumbers/LinearCongruence.java
+++ GeneratorRNG/src/randomNumbers/LinearCongruence.java
@@ -1,5 +1,33 @@
 package randomNumbers;
 
-public class LinearCongruence {
+public class LinearCongruence implements RandomGenerator{
+	private int seed,additiveConstant,multiplicativeConstant,currentXValue,amountToGenerate;
+
+	public void setConstants(int seed, int kappa, int adding,int generateNumber) {
+		setSeed(seed);
+		calculateMultiplicator(kappa);
+		additiveConstant=adding;
+		setAmountToGenerate(generateNumber);
+		
+	}
+	public void setAmountToGenerate(int amountToGenerate) {
+		this.amountToGenerate = Math.abs(amountToGenerate);
+	}
+	private void calculateMultiplicator(int kappa) {
+		multiplicativeConstant=4*kappa+1;
+	}
+	@Override
+	public float next() {
+		currentXValue=(multiplicativeConstant*currentXValue+additiveConstant)%amountToGenerate;
+		System.out.println(currentXValue);
+		return currentXValue/(amountToGenerate-1.0f);
+	}
+
+	@Override
+	public void setSeed(int seed) {
+		this.seed=seed;
+		currentXValue=this.seed;
+	}
+	
 
 }
