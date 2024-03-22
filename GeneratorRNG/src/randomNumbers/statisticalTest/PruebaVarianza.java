3d1d6c49eb73d3eccf48645f67bdc9e2f5fc8266
RomeroIsHere <guillermoromecu@gmail.com>
2024-03-22 00:26:31 -0600

    Refactored Prueba Varianza into Statistical Test Family

--- GeneratorRNG/src/randomNumbers/statisticalTest/PruebaVarianza.java
+++ GeneratorRNG/src/randomNumbers/statisticalTest/PruebaVarianza.java
@@ -5,49 +5,27 @@ import java.util.Scanner;
 import org.apache.commons.math3.distribution.ChiSquaredDistribution;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
-public class PruebaVarianza {
-	double alpha;
-    double [] nums;
-    int cantidad;
+public class PruebaVarianza extends StatisticalTest{
 	public PruebaVarianza(double alpha,double... data) {
-		this.alpha = alpha;
-    	nums=data;
+		super(alpha, data);
 	}
 
     public void test(Scanner sc) {
-        
-//Declaramos las variables que necesitaremos. Note que se establece como nivel de aceptación o confianza el de 95%.
-    	System.out.println("Escriba la cantidad de ri: ");
-        int cant_datos = sc.nextInt();
-        int grados_libertad = cant_datos - 1;
-        System.out.println("Escribe el valor de alfa: ");
-        double alfa = sc.nextDouble();
-
-        double[] datos = new double[cant_datos];
-
-        // Llenar el arreglo con números ingresados por el usuario
-        System.out.println("Ingrese los numeros ri:");
-        for (int i = 0; i < cant_datos; i++) {
-            System.out.print("Ingrese el numero " + (i + 1) + ": ");
-            datos[i] = sc.nextDouble();
-        }
+        int grados_libertad = data.length - 1;
 //Creamos objetos con las tablas de distribución normal y chi cuadrada, y un objeto que nos ayudará a obtener la media y la varianza.
 
         ChiSquaredDistribution chi = new ChiSquaredDistribution(grados_libertad);
         DescriptiveStatistics estadistica = new DescriptiveStatistics();
-
-
-        for (int i = 0; i < cant_datos; i++) {
-            estadistica.addValue(datos[i]);
-        }
+        for (double d : data) {
+			estadistica.addValue(d);
+		}
 
 //Declaramos las variables que necesitaremos en la prueba de varianza
         double varianza, li2, ls2;
         varianza = estadistica.getVariance();
-
 //Obtenemos los valores de la tabla de Chi cuadrada:
-        li2 = chi.inverseCumulativeProbability(alfa / 2) / (12 * grados_libertad);
-        ls2 = chi.inverseCumulativeProbability(1 - alfa / 2) / (12 * grados_libertad);
+        li2 = chi.inverseCumulativeProbability(alpha / 2) / (12 * grados_libertad);
+        ls2 = chi.inverseCumulativeProbability(1 - alpha / 2) / (12 * grados_libertad);
 
 //Mostramos los resultados en pantalla:
         System.out.println("---Prueba de varianza---");
